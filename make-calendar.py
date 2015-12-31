1585f89f5b5863605885650527ce490792f84b92
Patrik Ackland <patrikackland@gmail.com>
2015-12-31 11:31:17 +0100

    Added functionality

--- make-calendar.py
+++ make-calendar.py
@@ -1,14 +1,24 @@
+import Month
+
 class TexCalendar:
     def __init__(self, year, images):
         self.year = year
         self.images = images
         print "hello, world"
 
+    def stitch(self):
+        print "stitch"
+
     def make_pdf(self):
-        print "make"
+        for i in range(12):
+            Month(i, self.images[i]).make()
+        self.stitch()
 
     def add_holiday(self):
         print "holiday"
+
+    def add_birthday(self):
+        print "birthday"
 
 TexCalendar(2016, [
     "fuengirola.jpg",
