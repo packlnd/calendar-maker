213f0f8bf36f9fa2704228b363837fa2462506cc
Patrik Ackland <patrikackland@gmail.com>
2016-01-13 19:01:13 -0800

    More work on form, no drop

--- src/app.py
+++ src/app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+
 app = Flask(__name__)
 
 @app.route("/")
-def standard():
+def index():
     return render_template('index.html')
+
+@app.route("/upload",methods=["POST"])
+def upload():
+    print request.files
+    return 200
 
 if __name__ == "__main__":
     app.run()
