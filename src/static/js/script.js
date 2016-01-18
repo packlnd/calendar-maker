71922b93831ef56d17c44b9f596eb7740e67c310
Patrik Ackland <patrikackland@gmail.com>
2016-01-17 17:45:41 -0800

    Resources

---
+++ src/static/js/script.js
@@ -0,0 +1,18 @@
+$(function(ready) {
+    function readURL(input, month) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('img#img-'+month).attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("input[id^=browse]").change(function(){
+        console.log(this.id);
+        readURL(this, this.id.split("-")[1]);
+    });
+});
